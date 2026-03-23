67df7a2e812b233de8b3dc25aca341a827c91454
barysvla <vladislav.baryshev@outlook.com>
2026-03-23 19:06:37 +0100

    Rename compute_flow_accumulation to flow_acc

--- scripts/flow_accumulation.py
+++ scripts/flow_accumulation.py
@@ -15,7 +15,7 @@ D8_OFFSETS: list[tuple[int, int]] = [
 ]
 
 
-def compute_flow_accumulation(
+def flow_acc(
     *,
     dir_idx: np.ndarray | None = None,
     flow_weights: np.ndarray | None = None,
